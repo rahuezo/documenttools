8826cc9b43c3ea638550bf603c97b0a1f4d5316c
rahuezo <rahuezo@ucdavis.edu>
2018-03-29 15:45:44 -0700

    Added sanitize_files_list

--- documenttools/files/sanitizer.py
+++ documenttools/files/sanitizer.py
@@ -15,3 +15,7 @@ def sanitize_string(s):
         None.
     """
     return ' '.join(re.findall(r'[a-zA-Z0-9]+', s))
+
+
+def sanitize_files_list(files, extensions): 
+    return filter(lambda f: f.lower().endswith(map(lambda x: x.lower(), extensions)), files)
